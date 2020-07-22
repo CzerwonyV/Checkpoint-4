5f741ba7b3a0006227b96ae4c950e7959d21ade4
CzerwonyV <victor.veyrier@icloud.com>
2020-07-22 17:27:46 +0200

    feat(app): add route for home, contact and cv

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import CV from './components/CV/CV';
+import ContactMobile from './components/contact/contactMobile';
+import Home from './components/home/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/cv" component={CV} />
+          <Route path="/contact" component={ContactMobile} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
