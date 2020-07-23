ba700ee9f961cbfc2d55d77d086c94d24c8a832b
CzerwonyV <victor.veyrier@icloud.com>
2020-07-23 09:54:50 +0200

    feat(app): add route and token for admin

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,45 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import CV from './components/CV/CV';
 import ContactMobile from './components/contact/contactMobile';
 import Home from './components/home/home';
+import { connect } from 'react-redux';
+import ActuAdmin from './components/admin/actuAdmin';
+import SiteCardAdmin from './components/admin/siteCardAdmin';
+import LoginAdmin from './components/admin/loginAdmin';
+import NotFound from './components/admin/notfound';
+import DashboardAdmin from './components/admin/dashboardAdmin';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/cv" component={CV} />
-          <Route path="/contact" component={ContactMobile} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  withAuth = (Component) => {
+    const { token } = this.props;
+    const RedirectToError = () => (<Redirect to="/error" />);
+    return (token != null ? Component : RedirectToError);
+  };
+
+  render() {
+    const { withAuth } = this;
+    return (
+      <div className="AppContent">
+        <Router>
+          <Switch>
+            <Route path="/cv" component={CV} />
+            <Route path="/contact" component={ContactMobile} />
+            <Route path="/actuadmin" component={withAuth(ActuAdmin)} />
+            <Route path="/sitecardadmin" component={withAuth(SiteCardAdmin)} />
+            <Route path="/dashboardadmin" component={withAuth(DashboardAdmin)} />
+            <Route path="/adminlog" component={LoginAdmin} />
+            <Route path="/error" component={NotFound} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  token: state.reducer.token,
+});
+export default connect(mapStateToProps)(App);
