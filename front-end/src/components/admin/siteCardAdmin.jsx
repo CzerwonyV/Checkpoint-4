81df84ef73f619158a4520e933308dc75cbe1dac
CzerwonyV <victor.veyrier@icloud.com>
2020-07-24 01:37:34 +0200

    feat(sitecardadmin): add axios actu and map of this

--- front-end/src/components/admin/siteCardAdmin.jsx
+++ front-end/src/components/admin/siteCardAdmin.jsx
@@ -1,12 +1,68 @@
 import React from "react";
-import './admin.css';
+import axios from 'axios';
+import {
+    Card,
+    CardImg,
+    CardText,
+    CardBody,
+    CardTitle,
+    Button
+  } from "reactstrap";
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  import {
+    faGithub
+  } from "@fortawesome/free-brands-svg-icons";
 
-const SiteCardAdmin = () => {
-  return (
-    <div className="SiteCardAdminContent">
-        CA MARCHE
-    </div>
-  );
-};
-
-export default SiteCardAdmin;
+export default class SiteCardAdmin extends React.Component {
+    state = {
+        sitecard: []
+    }
+  
+    componentDidMount() {
+      axios.get(`http://localhost:8000/sitecard`)
+        .then(res => {
+          const sitecard = res.data;
+          this.setState({ sitecard });
+        })
+    }
+  
+    render() {
+      return (
+        <>
+          { this.state.sitecard.map(sitecard => { return (
+            <Card>
+                <CardImg
+                top
+                width="100%"
+                src={sitecard.imgurl}
+                alt={sitecard.name}
+                />
+            <CardBody>
+              <CardTitle>
+                <h4 className="TitleGroupCard">{sitecard.name}</h4>
+              </CardTitle>
+              <CardText>
+                {sitecard.textexplication}          
+              </CardText>
+              <a
+                href={sitecard.urlsite}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button className="TitleGroupCard BtnPortfolio">Voir le site</Button>
+              </a>
+              <a
+                href={sitecard.urlgithub}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button id="SpaceButton" className="TitleGroupCard BtnPortfolio"><FontAwesomeIcon icon={faGithub} id="logoGithubCard" /></Button>
+              </a>
+            </CardBody>
+          </Card>
+          )
+          })}
+        </>
+      )
+    }
+  }
