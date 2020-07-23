341c7026152b33f369bd962dbbc8d7ebb0ca82a7
CzerwonyV <victor.veyrier@icloud.com>
2020-07-23 09:55:25 +0200

    feat(index): add redux and provider

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Reducer from './redux/Reducer';
+
+const rootReducer = combineReducers({
+  reducer: Reducer,
+  // <other reducers here>
+});
+const store = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
