47451070dd7c5b6fb81a161bb1550dee0627b15b
CzerwonyV <victor.veyrier@icloud.com>
2020-07-24 01:34:55 +0200

    feat(addactu): add modal in dashboard for add actu

--- front-end/src/components/admin/modalAdd/addActu.jsx
+++ front-end/src/components/admin/modalAdd/addActu.jsx
@@ -1,24 +1,72 @@
 import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { Input, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import './modalAdd.css';
 
 const AddActu = () => {
+    const [text, setText] = useState('');
+    const history = useHistory();
+    
+    const onChangeText = (e) => {
+        setText(
+        e.target.value,
+        );
+    };
+    
+    function submitForm(e) {
+        e.preventDefault();
+        const url = 'http://localhost:8000/actu/add';
+    
+        axios.post(url, {
+          text,
+        })
+        .then((res) => res.data)
+        .then((res) => {
+            alert(`Le texte a bien été enregistré !`);
+            axios.post('http://localhost:8000/actu', {
+                text
+                })
+                .then((res) => res.data)
+                .then((res) => {
+                    history.push('/dashboardadmin');
+                });
+        })
+    
+        .catch(() => {
+            alert(`Erreur lors de l'ajout d'un text`);
+        });
+    }
 
   const [modal, setModal] = useState(false);
 
   const toggle = () => setModal(!modal);
 
+  const closeBtn = <button className="close CloseModal" onClick={toggle}>&times;</button>;
+
   return (
     <div>
-      <Button className="BtnModalAdd" onClick={toggle}>Ajoutez une actu</Button>
+      <Button className="BtnModalAdd BtnAddMobile" onClick={toggle}>Ajoutez une actu</Button>
       <Modal isOpen={modal} toggle={toggle} className="" >
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalHeader toggle={toggle} close={closeBtn}>Ajoutez un actu</ModalHeader>
         <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <form onSubmit={submitForm}>
+                <div className="form-data DivInput">
+                <label htmlFor="text" className="LabelForm">Le text de l'actu </label>
+                <Input
+                    type="textarea"
+                    id="text"
+                    name="text"
+                    onChange={onChangeText}
+                    value={text}
+                    required
+                />
+                </div>
+                <div className="BtnAddDiv">
+                    <Button className="BtnAdd" type="submit" onClick={toggle}>Ajoutez</Button>
+                </div>
+            </form>
         </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" onClick={toggle}>Ajoutez</Button>
-        </ModalFooter>
       </Modal>
     </div>
   );
