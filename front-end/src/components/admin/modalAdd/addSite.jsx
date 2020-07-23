76f28987e90d389d3bb9dc5b1a3d9950d8e98147
CzerwonyV <victor.veyrier@icloud.com>
2020-07-24 01:35:19 +0200

    feat(addsite): add modal in dashboard for add site

--- front-end/src/components/admin/modalAdd/addSite.jsx
+++ front-end/src/components/admin/modalAdd/addSite.jsx
@@ -1,40 +1,67 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
-import { Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Input, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import './modalAdd.css';
 
 const AddSite = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [imgurl, setImgUrl] = useState('');
+    const [name, setName] = useState('');
+    const [textexplication, setTextExplication] = useState('');
+    const [urlsite, setUrlSite] = useState('');
+    const [urlgithub, setUrlGitHub] = useState('');
     const history = useHistory();
     
-    const onChangeUsername = (e) => {
-        setUsername(
+    const onChangeImgUrl = (e) => {
+        setImgUrl(
         e.target.value,
         );
     };
-    
-    const onChangePassword = (e) => {
-        setPassword(
+
+    const onChangeName = (e) => {
+        setName(
+        e.target.value,
+        );
+    };
+
+    const onChangeTextExplication = (e) => {
+        setTextExplication(
+        e.target.value,
+        );
+    };
+
+    const onChangeUrlSite = (e) => {
+        setUrlSite(
+        e.target.value,
+        );
+    };
+
+    const onChangeUrlGitHub = (e) => {
+        setUrlGitHub(
         e.target.value,
         );
     };
     
     function submitForm(e) {
         e.preventDefault();
-        const url = 'http://localhost:8000/users/register';
+        const url = 'http://localhost:8000/sitecard/add';
     
         axios.post(url, {
-        username,
-        password,
+          imgurl,
+          name,
+          textexplication,
+          urlsite,
+          urlgithub
         })
         .then((res) => res.data)
         .then((res) => {
-            alert(`Vôtre compte ${res.username} a bien été enregistré !`);
-            axios.post('http://localhost:8000/users/login', {
-                username,
-                password,
+            alert(`Le texte a bien été enregistré !`);
+            axios.post('http://localhost:8000/sitecard', {
+                imgurl,
+                name,
+                textexplication,
+                urlsite,
+                urlgithub
                 })
                 .then((res) => res.data)
                 .then((res) => {
@@ -43,7 +70,7 @@ const AddSite = () => {
         })
     
         .catch(() => {
-            alert(`Erreur lors de l'ajout d'un compte : ${e.message}`);
+            alert(`Erreur lors de l'ajout des données`);
         });
     }
 
@@ -51,40 +78,75 @@ const AddSite = () => {
 
   const toggle = () => setModal(!modal);
 
+  const closeBtn = <button className="close CloseModal" onClick={toggle}>&times;</button>;
+
   return (
     <div>
-      <Button className="BtnModalAdd" onClick={toggle}>Ajoutez un site</Button>
+      <Button className="BtnModalAdd BtnAddMobile" onClick={toggle}>Ajoutez un site</Button>
       <Modal isOpen={modal} toggle={toggle} className="" >
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalHeader toggle={toggle} close={closeBtn}>Ajoutez un site</ModalHeader>
         <ModalBody>
             <form onSubmit={submitForm}>
                 <div className="form-data DivInput">
-                <label htmlFor="username" className="LabelForm">Nom d&apos;utilisateur</label>
+                <label htmlFor="imgurl" className="LabelForm">Le lien de l'image du site</label>
                 <Input
                     type="text"
-                    id="username"
-                    name="username"
-                    onChange={onChangeUsername}
-                    value={username}
+                    id="imgurl"
+                    name="imgurl"
+                    onChange={onChangeImgUrl}
+                    value={imgurl}
                     required
                 />
                 </div>
                 <div className="form-data DivInput">
-                <label htmlFor="password" className="LabelForm">Mot de passe</label>
+                <label htmlFor="name" className="LabelForm">Le nom du site</label>
                 <Input
-                    type="password"
-                    id="password"
-                    name="password"
-                    onChange={onChangePassword}
-                    value={password}
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={onChangeName}
+                    value={name}
                     required
                 />
                 </div>
+                <div className="form-data DivInput">
+                <label htmlFor="textexplication" className="LabelForm">Le text explicatif du site</label>
+                <Input
+                    type="textarea"
+                    id="textexplication"
+                    name="textexplication"
+                    onChange={onChangeTextExplication}
+                    value={textexplication}
+                    required
+                />
+                </div>
+                <div className="form-data DivInput">
+                <label htmlFor="urlsite" className="LabelForm">L'url du site</label>
+                <Input
+                    type="text"
+                    id="urlsite"
+                    name="urlsite"
+                    onChange={onChangeUrlSite}
+                    value={urlsite}
+                    required
+                />
+                </div>
+                <div className="form-data DivInput">
+                <label htmlFor="urlgithub" className="LabelForm">L'url du dépôt GitHub</label>
+                <Input
+                    type="text"
+                    id="urlgithub"
+                    name="urlgithub"
+                    onChange={onChangeUrlGitHub}
+                    value={urlgithub}
+                    required
+                />
+                </div>
+                <div className="BtnAddDiv">
+                    <Button className="BtnAdd" type="submit" onClick={toggle}>Ajoutez</Button>
+                </div>
             </form>
         </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" type="submit" onClick={toggle}>Ajoutez</Button>
-        </ModalFooter>
       </Modal>
     </div>
   );
