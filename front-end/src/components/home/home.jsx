bcdd6dd0f3af27e7927ccc77da3e8d6efb183caf
CzerwonyV <victor.veyrier@icloud.com>
2020-07-24 01:40:43 +0200

    feat(home): add actuContent siteCardContent

--- front-end/src/components/home/home.jsx
+++ front-end/src/components/home/home.jsx
@@ -3,6 +3,8 @@ import TwitterTimeline from "react-twitter-embedded-timeline";
 import BtnActu from "../button/btnActu";
 import "./home.css";
 import NavBarCV from "../navbar/navbar";
+import ActuContent from "./actuContent";
+import SiteCardContent from "./siteCardContent";
 
 const Home = () => {
   return (
@@ -12,14 +14,14 @@ const Home = () => {
         <div className="ActuContent">
           <div className="ActuGauche">
             <BtnActu />
-            
+            <ActuContent />
           </div>
           <div className="ActuDroite">
             <TwitterTimeline className="TLTwitter" widgetId="TLContent" user="CzerwonyV" />
           </div>
         </div>
         <div className="PortFolioContentCard">
-
+          <SiteCardContent />
         </div>
       </div>
     </>
